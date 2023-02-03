099fa66480b2628fbc25e26469501105b2d165c6
Echo <576520923@qq.com>
2023-02-03 23:05:18 +0800

    hot-fix first commit

--- src/main/java/org/echo/GitTest.java
+++ src/main/java/org/echo/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("666");
+        System.out.println("hello 666");
     }
 }
