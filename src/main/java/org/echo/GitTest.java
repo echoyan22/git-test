7487f536138c8e1d0695e0051ba3757a71af6387
Echo <576520923@qq.com>
2023-02-04 00:38:12 +0800

    gitee push commit

--- src/main/java/org/echo/GitTest.java
+++ src/main/java/org/echo/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello 666");
         System.out.println("master test!");
         System.out.println("push test!");
+        System.out.println("gitee push test!");
     }
 }
