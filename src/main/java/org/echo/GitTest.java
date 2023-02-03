2f5404a4d7c9f4c823cb3389282ea701cd7f1b08
Echo <576520923@qq.com>
2023-02-03 23:12:40 +0800

    master second commit

--- src/main/java/org/echo/GitTest.java
+++ src/main/java/org/echo/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("666");
         System.out.println("hello 666");
+        System.out.println("master test!");
     }
 }
